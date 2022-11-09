9bdc8b0254f2487ecf3a9ac707ec9928cf7be3be
ramachandra549 <ramachandranayak549@gmail.com>
2022-11-09 23:02:15 +0530

    Initial commit

---
+++ Palindrome Program/Palindrome.java
@@ -0,0 +1,28 @@
+package com.programming;
+import java.util.Scanner;
+public class Palindrome {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int num=sc.nextInt();
+		int org_num=num;
+		int rev=0;
+		while(num!=0)
+		{
+			rev=rev*10 + num%10;
+			num=num/10;
+		}
+		if(rev==org_num)
+		{
+			System.out.println(rev+" : its a palindrome number");
+		}
+		else
+		{
+			System.out.println(rev+" : it is Not  Palindrome");
+		}
+		
+		
+	}
+
+}
